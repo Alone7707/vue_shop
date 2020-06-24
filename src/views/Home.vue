8194ea4e7097052abc85a9b788e8eea7e3b54c82
alone7707 <770733914@qq.com>
2020-06-24 22:12:29 +0800

    '登陆注册'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home组件
+    <div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    logout() {
+      // 清空token
+      window.sessionStorage.clear()
+      // 通过Promise延迟一秒跳转回登陆页面
+      this.$router.push('/login')
+    }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
